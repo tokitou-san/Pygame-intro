e1983a112fcc809d47e9792cfe5cbb019ec759cb
tokitou-san <tokitousan2005@gmail.com>
2023-08-22 17:43:22 +0530

    feat: add sky and ground surface

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ screen = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Hello pygame")
 clock = pygame.time.Clock()
 
-surface = pygame.Surface((50, 50))
-surface.fill("blue")
+sky_surface = pygame.image.load("graphics/Sky.png")
+sky_surface = pygame.transform.scale(sky_surface, (1000, 600))
+
+ground_surface = pygame.image.load("graphics/Ground.png")
+ground_surface = pygame.transform.scale(ground_surface, (1000, 100))
 
 while True:
 	for event in pygame.event.get():
@@ -15,7 +18,8 @@ while True:
 			pygame.quit()
 			exit()
 
-	screen.blit(surface, (200, 100))
+	screen.blit(sky_surface, (0, 0))
+	screen.blit(ground_surface, (0, 500))
 
 	pygame.display.update()
 	clock.tick(60)
