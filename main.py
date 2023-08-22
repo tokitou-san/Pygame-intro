fe5bea0dfa347409f35e4d42f966b8edf36e5b4a
tokitou-san <tokitousan2005@gmail.com>
2023-08-22 21:03:50 +0530

    feat: add Player surface and remove Text surface

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Hello pygame")
 clock = pygame.time.Clock()
-landing_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 sky_surface = pygame.transform.scale(sky_surface, (1000, 600))
@@ -13,11 +12,12 @@ sky_surface = pygame.transform.scale(sky_surface, (1000, 600))
 ground_surface = pygame.image.load("graphics/Ground.png").convert()
 ground_surface = pygame.transform.scale(ground_surface, (1000, 100))
 
-landing_text = landing_font.render("My Game", True, "Black")
-
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (1100, 500))
 snail_speed = 4
+
+player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+player_rect = player_surface.get_rect(bottomleft = (100, 500))
 
 while True:
 	for event in pygame.event.get():
@@ -27,13 +27,13 @@ while True:
 
 	screen.blit(sky_surface, (0, 0))
 	screen.blit(ground_surface, (0, 500))
-	screen.blit(landing_text, (450, 100))
 
 	if snail_rect.right < 0:
 		snail_rect.left = 1100
 	else:
 		snail_rect.left -= snail_speed
 	screen.blit(snail_surface, snail_rect)
+	screen.blit(player_surface, player_rect)
 
 	pygame.display.update()
 	clock.tick(60)
