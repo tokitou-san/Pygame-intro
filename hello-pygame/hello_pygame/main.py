c70a897bbdd2155e937bdb934b7968d8e43feca9
tokitou-san <tokitousan2005@gmail.com>
2023-08-22 06:51:55 +0530

    exit/quit operation for window

--- hello-pygame/hello_pygame/main.py
+++ hello-pygame/hello_pygame/main.py
@@ -1,4 +1,13 @@
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((1000, 600))
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			exit()
+
+	pygame.display.update()
