fed69ab7f42da5e5671efccf97241cf3cb449499
tokitou-san <tokitousan2005@gmail.com>
2023-08-23 08:07:31 +0530

    feat: add Score surface with Font method and Rect

--- main.py
+++ main.py
@@ -19,24 +19,25 @@ snail_speed = 4
 player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
 player_rect = player_surface.get_rect(bottomleft = (100, 500))
 
+score_surface = pygame.font.Font("font/Pixeltype.ttf", 50)
+score_surface = score_surface.render("Score: 0", False, "Black")
+score_rect = score_surface.get_rect(center = (500, 100))
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit()
 
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			if player_rect.collidepoint(event.pos): print ("Collide")
-
 	screen.blit(sky_surface, (0, 0))
 	screen.blit(ground_surface, (0, 500))
 
 	snail_rect.x -= snail_speed
-	if snail_rect.right < 0:
-		snail_rect.left = 1100
+	if snail_rect.right < 0: snail_rect.left = 1100
 
 	screen.blit(snail_surface, snail_rect)
 	screen.blit(player_surface, player_rect)
+	screen.blit(score_surface, score_rect)
 
 	pygame.display.update()
 	clock.tick(60)
