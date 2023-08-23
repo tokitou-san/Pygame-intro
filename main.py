3c4bcd1ede644455878de83009a28744e3369c82
tokitou-san <tokitousan2005@gmail.com>
2023-08-23 16:24:20 +0530

    feat: add functional Score board

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pygame.display.set_caption("Hello pygame")
 clock = pygame.time.Clock()
 text_surface = pygame.font.Font("font/Pixeltype.ttf", 50)
 game_state = "playing"
+game_time = 0
 
 # Sky
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
@@ -26,9 +27,11 @@ player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_
 player_rect = player_surface.get_rect(bottomleft = (100, 500))
 player_gravity = 0
 
-# Score
-score_surface = text_surface.render("Score: 0", False, "#5A5A5A")
-score_rect = score_surface.get_rect(center = (500, 100))
+def update_score():
+	current_time = int(pygame.time.get_ticks() / 1000) - game_time
+	score_surface = text_surface.render(f"Score: {current_time}", False, "#5A5A5A")
+	score_rect = score_surface.get_rect(center = (500, 100))
+	screen.blit(score_surface, score_rect)
 
 while True:
 	for event in pygame.event.get():
@@ -42,12 +45,14 @@ while True:
 			elif event.key == pygame.K_SPACE and game_state == "over":
 				game_state = "playing"
 				snail_rect.left = 1000
+				game_time = int(pygame.time.get_ticks() / 1000)
 
 	if game_state == "playing":
 		screen.blit(sky_surface, (0, 0))
 		screen.blit(ground_surface, (0, 500))
-		screen.blit(score_surface, score_rect)
-		
+		# Score
+		update_score()
+
 		# Snail
 		snail_rect.x -= snail_speed
 		if snail_rect.right < 0: snail_rect.left = 1100
